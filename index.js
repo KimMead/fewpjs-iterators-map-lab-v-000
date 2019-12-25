7f41265bac88aa4840884ba172270562741353d5
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-12-25 02:35:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ const tutorials = [
 function titleCased() {
   return tutorials.map(function(tutorial) {
     return tutorial.split(" ").map(t => t[0].toUpperCase() + t.slice(1)).join(" ");
-    
+
   });
 }
